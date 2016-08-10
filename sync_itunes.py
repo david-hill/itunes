fd200086f92f87ca323648c27ea9a78696bd00e9
Dave <daweidave@hotmail.com>
2016-08-09 23:49:40 -0400

    Initial commit

---
+++ sync_itunes.py
@@ -0,0 +1,154 @@
+import re
+import MySQLdb
+import sys  
+import musicbrainzngs
+from htmlentitydefs import name2codepoint as n2cp
+reload(sys)  
+sys.setdefaultencoding('utf8')
+debug=0
+tot=0
+db = MySQLdb.connect("localhost","musicbrainz","musicbrainz","musicbrainz" )
+c = db.cursor()
+c.execute("SET NAMES utf8;") 
+c.execute("SET CHARACTER SET utf8;")
+file = open('iTunes Music Library.xml')
+fields = [ 'Artist', 'Location', 'Album', 'Year' ]
+dict = {}
+artists = {}
+def decodeHtmlentities(string):
+    rc=-1
+    try:
+      entity_re = re.compile("&(#?)(\d{1,5}|\w{1,8});")
+      def substitute_entity(match):
+          ent = match.group(2)
+          if match.group(1) == "#":
+              return unichr(int(ent))
+          else:
+              cp = n2cp.get(ent)
+              if cp:
+                  return unichr(cp)
+              else:
+                  return match.group()
+      rc=entity_re.subn(substitute_entity, string)[0]
+    except:
+      print sys.exc_info()
+      pass
+    return rc
+def count_itunes_locations():
+  tot=0
+  for line in file:
+    rs="<key>Location</key><.*>(.*)</.*>"
+    m = re.search(rs, line)
+    if m:
+      tot+=1
+      if debug:
+        print tot
+  return tot
+def extract_from_itunes():
+  nbr_loc=count_itunes_locations()
+  print "Extracting " + str(nbr_loc) + " songs..."
+  album='empty'
+  artist='empty'
+  partist='empty'
+  pyear=0
+  year=0
+  pinc=0
+  inc=0
+  prog=0
+  file.seek(0)
+  for line in file:
+    if debug:
+      print line
+    for field in fields:
+      rs="<key>" + field + "</key><.*>(.*)</.*>"
+      m = re.search(rs, line)
+      if m:
+        if field is not 'Location':
+          dict.update({field:m.group(1)})
+        if field is 'Location':
+          prog+=1 
+          sys.stdout.write("\rProgress %.2f%s [%d/%d]" % (float(prog) / float(nbr_loc) * 100, '%', prog, nbr_loc))
+          sys.stdout.flush()
+          for f in fields:
+            if f is not 'Location':
+              try:
+                if debug:
+                  print dict[f]
+              except:
+                pass
+          pdict=dict
+          partist=artist
+          palbum=album
+          pyear=year
+          artist=decodeHtmlentities(dict['Artist']).lower()
+          album=decodeHtmlentities(dict['Album']).lower()
+          try:
+            year=decodeHtmlentities(dict['Year'])
+          except:
+            year='0'
+          if (partist != artist or palbum != album):
+            for p in artists.keys():
+              if p == partist:
+                if palbum not in artists[ partist ].keys():
+                  artists[ partist ][ palbum ]=pdict.copy()
+                else:
+                  artists[ partist ][ palbum ]['inc'] += inc
+            if partist not in artists.keys():
+              artists[ partist ] = {}
+              artists[ partist ][ palbum ]=pdict.copy()
+            pinc=inc
+            inc=1
+            if (partist != "Unknown" and palbum != "Unknown"):
+              eartist=MySQLdb.escape_string(partist)
+              ealbum=MySQLdb.escape_string(palbum)
+              sql = "select present from musicbrainz where artist like '" + eartist + "' and name like '" + ealbum  +"';"
+              if debug:
+                print sql
+              r=c.execute(sql)
+              if c.rowcount:
+                (result,)=c.fetchone()
+                if (result != pinc):
+                  print "WARNING: result != pinc"
+                  sql = "update musicbrainz set present=" + str(artists[ partist ][ palbum ]['inc']) + " where artist like '" + eartist + "' and name like '" + ealbum  +"';"
+                  if debug:
+                    print sql
+                  sys.stdout.write("\n")
+                  sys.stdout.flush()
+                  r=c.execute(sql)
+                  rall = c.fetchall()
+              else:
+                if (palbum != 'empty' and partist != 'empty'):
+                  eartist=MySQLdb.escape_string(partist)
+                  ename=MySQLdb.escape_string(palbum)
+                  etype="Album"
+                  eyear=pyear
+                  #sql = "insert into musicbrainz values(" + str(pinc) + ", '" + eartist + "','" + ename + "','" + etype + "','" + eyear + "',NULL);"
+                  #print sql
+                  #sys.stdout.write("\n")
+                  #sys.stdout.flush()
+                  #print sql
+                  #r=c.execute(sql)
+                  #rall = c.fetchall()
+          else:
+            inc+=1
+            dict.update({'inc': inc})
+                
+def print_artists_details():
+  print "======================"
+  try:
+    for p in artists.keys():
+      print p
+      print "---"
+      try:
+        for q in artists[p]:
+          print q
+          print "|"
+      except:
+        print sys.exc_info()
+        pass
+  except:
+    print sys.exc_info()
+    pass
+
+extract_from_itunes()
+print_artists_details()
---
+++ sync_musicbrainz.py
@@ -0,0 +1,108 @@
+import re
+import MySQLdb
+import sys  
+import musicbrainzngs
+from htmlentitydefs import name2codepoint as n2cp
+reload(sys)  
+sys.setdefaultencoding('utf8')
+debug=0
+tot=0
+db = MySQLdb.connect("localhost","musicbrainz","musicbrainz","musicbrainz" )
+c = db.cursor()
+c.execute("SET NAMES utf8;") 
+c.execute("SET CHARACTER SET utf8;")
+file = open('iTunes Music Library.xml')
+fields = [ 'Artist', 'Location', 'Album', 'Year' ]
+dict = {}
+artists = {}
+
+myid=0
+def fetch_artist(artist,cptdone,artistcpt):
+  myid=0
+  print("%s\t\tTotal Progress %.2f %s [%d/%d]" % ( artist, float(cptdone) / float(artistcpt) * 100, '%', cptdone, artistcpt))
+  musicbrainzngs.set_useragent('itunesync', '0.0.1', 'contact=daweidave@hotmail.com')
+  try:
+    result = musicbrainzngs.search_artists(artist=artist, type="group")
+    for artist in result['artist-list']:
+      try:
+        if debug:
+          print(u"{id}: {name}".format(id=artist['id'], name=artist["name"]))
+        if not myid:
+          myid=artist["id"]
+      except:
+        print sys.exc_info()
+        pass
+  except:
+    pass
+  return myid
+def fetch_releases(artist,myid):
+  try:
+    result = musicbrainzngs.browse_release_groups(myid, includes=["release-group-rels"], offset=0)
+    max=result["release-group-count"]
+  except:
+    print sys.exc_info()
+    max=-1
+    pass
+  offset=0
+  while (offset<max):
+    try:
+      result = musicbrainzngs.browse_release_groups(myid, includes=["release-group-rels"], offset=offset)
+      max=result["release-group-count"]
+      for release in result["release-group-list"]:
+        try:
+          if 'type' not in release.keys():
+            release.update({'type': 'none'})
+          if debug:
+            print("{title} {date} ({type})".format(title=release["title"], type=release["type"], date=release["first-release-date"]))
+          eartist=MySQLdb.escape_string(artist)
+          ealbum=MySQLdb.escape_string(release["title"])
+          sql = "select present from musicbrainz where artist like '" + eartist + "' and name like '" + ealbum  +"' and type like '" + release["type"] + "';"
+          if debug:
+            print sql
+          r=c.execute(sql)
+          if c.rowcount:
+            (result,)=c.fetchone()
+            sql = "update musicbrainz set last_updated=CURRENT_TIMESTAMP where artist like '" + eartist + "' and name like '" + ealbum + "' and type like '" + release["type"] + "' and year like '" + release["first-release-date"] + "';"
+            if debug:
+              print sql
+            r=c.execute(sql)
+            rall = c.fetchall()
+          else:
+            sql = "insert into musicbrainz values(0, '" + eartist + "','" + ealbum + "','" + release["type"] + "','" + release["first-release-date"] + "', CURRENT_TIMESTAMP);"
+            if debug:
+              print sql
+            r=c.execute(sql)
+            rall = c.fetchall()
+          sys.stdout.write("\rProgress %.2f%s" % (float(offset) / float(max) * 100, '%'))
+          sys.stdout.flush()
+        except:
+          print sys.exc_info()
+          pass
+      offset+=25
+    except:
+      print sys.exc_info()
+      pass
+  sys.stdout.write("\rProgress %.2f%s\n" % (float('1') / float('1') * 100, '%'))
+  sys.stdout.flush()
+
+def sync_musicbrainz():
+  cptdone=0
+  sql = "select count(distinct artist) from musicbrainz where last_updated is null or last_updated < DATE_SUB(NOW(), INTERVAL 7 DAY);"
+  r=c.execute(sql)
+  (artistcpt,)=c.fetchone()
+  if artistcpt:
+    sql = "select artist from musicbrainz where last_updated is null or last_updated < DATE_SUB(NOW(), INTERVAL 7 DAY) group by artist;"
+    r=c.execute(sql)
+    results=c.fetchall()
+    for result in results:
+      myid=fetch_artist(result[0],cptdone,artistcpt)
+      if myid:
+        fetch_releases(result[0],myid)
+      cptdone+=1
+    sql = "select count(distinct artist) from musicbrainz where last_updated is null or last_updated < DATE_SUB(NOW(), INTERVAL 7 DAY);"
+    r=c.execute(sql)
+    (artistcpt,)=c.fetchone()
+    print "WARNING: %d artists were not updated!  Cleaning them..."
+    sql = "update musicbrainz set last_updated=CURRENT_TIMESTAMP musicbrainz where last_updated is null or last_updated < DATE_SUB(NOW(), INTERVAL 7 DAY);"
+    r=c.execute(sql)
+sync_musicbrainz()
